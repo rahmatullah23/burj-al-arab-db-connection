fb0b225f126eef5477cf82489af79189eb728c3a
rahmatullah23 <tomalg@gmail.com>
2020-10-02 07:42:57 +0300

    first commit

---
+++ src/components/Book/Book.js
@@ -0,0 +1,84 @@
+import React, { useContext, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { Button } from '@material-ui/core';
+import Booking from '../Booking/Booking';
+
+
+const Book = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    const [selectedDate, setSelectedDate] = useState({
+        checkIn:new Date(),
+        checkOut:new Date()
+    });
+
+  const handleCheckInDate = (date) => {
+      const newDates = {...selectedDate}
+      newDates.checkIn = date;
+    setSelectedDate(newDates);
+  };
+  const handleCheckOutDate = (date) => {
+      const newDates = {...selectedDate}
+      newDates.checkOut = date;
+    setSelectedDate(newDates);
+  };
+  const handleBooking = () => {
+      const newBooking = {...loggedInUser, ...selectedDate}
+      fetch('http://localhost:4050/addBooking',{
+          method : 'POST',
+          headers: {'content-type': 'application/json'},
+          body: JSON.stringify(newBooking)
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+      })
+
+  }
+    const {bedType} = useParams();
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1> Hello {loggedInUser.name}! Let's book a {bedType} Room.</h1>
+            <p>Want a <Link to="/home">different room?</Link> </p>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Check In Date "
+          value={selectedDate.checkIn}
+          onChange={handleCheckInDate}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Check Out Date "
+          format="dd/MM/yyyy"
+          value={selectedDate.checkOut}
+          onChange={handleCheckOutDate}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </Grid>
+      <Button onClick={handleBooking} variant="contained" color="primary">Book Now</Button>
+    </MuiPickersUtilsProvider>
+    <Booking></Booking>
+
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/components/Booking/Booking.js
@@ -0,0 +1,33 @@
+import { set } from 'date-fns/esm';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
+
+const Booking = () => {
+    const [bookings, setBookings] = useState([]);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    useEffect(() => {
+        fetch('http://localhost:4050/bookings?email='+loggedInUser.email,{
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                authorization : `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data =>setBookings(data))
+    },[])
+    
+    return (
+        <div>
+            <h3>Booking list {bookings.length}</h3>
+          {
+              bookings.map(book => <li>{book.name} 
+                                        from {new Date(book.checkIn).toDateString('dd/MM/yy')} 
+                                        to {book.checkOut}
+                                   </li>)
+          }
+        </div>
+    );
+};
+
+export default Booking;
